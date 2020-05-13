0da9917d005843fc2b8f10b34e295926080f9906
seowons <43734342+seowons@users.noreply.github.com>
2020-05-14 01:24:02 +0900

    Update ImageDataGenerator.py

--- ImageDataGenerator.py
+++ ImageDataGenerator.py
@@ -32,11 +32,14 @@ img = load_img('C:\\Users\\user\\Desktop\\test1\\udon (1).jpg')  # PIL 이미지
 x = img_to_array(img)  # (3, 150, 150) 크기의 NumPy 배열
 x = x.reshape((1,) + x.shape)  # (1, 3, 150, 150) 크기의 NumPy 배열
 
+
 # 아래 .flow() 함수는 임의 변환된 이미지를 배치 단위로 생성해서
-# 지정된 `preview/` 폴더에 저장합니다.
+#C:\\Users\\user\\Desktop\\test1\\preview 에 저장
 i = 0
 for batch in datagen.flow(x, batch_size=1,
                           save_to_dir='C:\\Users\\user\\Desktop\\test1\\preview', save_prefix='udon', save_format='jpeg'):
     i += 1
     if i > 20:
         break  # 이미지 20장을 생성하고 마칩니다
+
+        
